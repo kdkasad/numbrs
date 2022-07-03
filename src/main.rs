7077d5982c8e6f7722262dd5f6a5fd08eaa064e8
Kian Kasad <kian@kasad.com>
2022-07-03 16:05:43 -0700

    Implement reading and processing input from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,41 @@
+use crate::parser::Parser;
+use rustyline::{error::ReadlineError, Editor};
+
 mod ast;
+mod parser;
 mod scanner;
 
+const PROMPT: &str = "> ";
+
+const COLOR_ERR: &str = "\x1b[1;31m";
+const COLOR_RST: &str = "\x1b[m";
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: add completion helper
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        match rl.readline(PROMPT) {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+
+                let p = Parser::new(&line);
+                match p.parse() {
+                    Ok(ast) => println!("{:?}", ast),
+                    Err(e) => println!("{}Error:{} {}", COLOR_ERR, COLOR_RST, e),
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(e) => {
+                println!("{}Error:{} {:?}", COLOR_ERR, COLOR_RST, e);
+                break;
+            }
+        }
+    }
 }
