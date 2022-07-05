3bfb2ba35e372ca88f85b7f7ed33030bf9856cb2
Kian Kasad <kian@kasad.com>
2022-07-05 16:51:09 -0700

    Add exponent/power operation to BigDecimal struct

--- src/bigdecimal_helper.rs
+++ src/bigdecimal_helper.rs
@@ -15,10 +15,52 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+use bigdecimal::BigDecimal;
+
 #[allow(unused_macros)]
 macro_rules! bigdec {
     ($lit:literal) => {{
         use std::str::FromStr;
         bigdecimal::BigDecimal::from_str(stringify!($lit)).expect("Invalid decimal float literal")
     }};
+}
+
+pub trait BigDecimalPowExt {
+    fn pow(self, exp: u32) -> BigDecimal;
+}
+
+impl BigDecimalPowExt for BigDecimal {
+    fn pow(self, exp: u32) -> Self {
+        let (mut digits, mut scale) = self.into_bigint_and_exponent();
+        digits = digits.pow(exp);
+        scale *= exp as i64;
+        BigDecimal::new(digits, scale)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bigdecimal_pow() {
+        macro_rules! case {
+            ( $a:literal, $b:literal ) => {
+                (bigdec!($a), $a as f64, $b)
+            };
+        }
+
+        let cases: [(BigDecimal, f64, u32); 4] = [
+            case!(2.5, 2),
+            case!(2, 2),
+            case!(10.2349, 2),
+            case!(29384293, 2),
+        ];
+
+        for (bd, f, exp) in cases {
+            let bd = bd.pow(exp);
+            let f = f.powf(exp.into());
+            assert_eq!(bd.to_string(), f.to_string());
+        }
+    }
 }
