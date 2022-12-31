0cdb6a4db0754967d742b01e9bb346b211f60bd9
Kian Kasad <kian@kasad.com>
2022-12-31 15:05:44 -0800

    Wrap prefix and suffix lists in accessor functions
    The list of standard prefixes and standard suffixes are both static
    objects. The prefix list must be initialized at runtime, so it must be
    wrapped in a lazy initializer. To avoid making the public library API
    expose the implementation of this, we wrap both lists in accessor
    functions which return a static slice.
    
    This commit changes the public API and therefore requires a minor
    version bump.

--- src/affixes.rs
+++ src/affixes.rs
@@ -21,8 +21,8 @@ along with Numbrs.  If not, see <https://www.gnu.org/licenses/>.
 
 use std::collections::HashMap;
 
-use lazy_static::lazy_static;
 use num::BigRational;
+use once_cell::sync::Lazy;
 
 use crate::{ast::Value, rat_util_macros::rat, unit::Unit};
 
@@ -53,7 +53,17 @@ impl Prefix {
         &self.scale
     }
 
-    /// Get the prefix's standalone status.
+    /// Whether or not the [`Prefix`] also acts as a standalone variable
+    /// representing its scale as a numeric value.
+    ///
+    /// For example, the prefix `kilo` is standalone, but `k` is not:
+    /// ```txt
+    /// $ numbrs
+    /// > kilo
+    /// 1000
+    /// > k
+    /// Error: Evaluation error: `k` not defined
+    /// ```
     pub fn standalone(&self) -> bool {
         self.standalone
     }
@@ -65,7 +75,8 @@ pub fn resolve_unit(name: &str, env: &HashMap<String, Value>) -> Option<Unit> {
         return Some(unit);
     }
 
-    for (suffix, substitution) in SUFFIX_MAP.iter() {
+    // TODO: don't transform units which don't support suffixes, e.g. `kg`
+    for (suffix, substitution) in standard_suffixes() {
         if let Some(base) = name.strip_suffix(suffix) {
             let mut new_name = String::with_capacity(name.len());
             new_name.push_str(base);
@@ -86,7 +97,7 @@ fn try_get_unit(name: &str, env: &HashMap<String, Value>) -> Option<Unit> {
     }
 
     // Search for existing unit by stripping available prefixes
-    for prefix in (*PREFIXES).iter() {
+    for prefix in standard_prefixes() {
         if let Some(s) = name.strip_prefix(prefix.text()) {
             if let Some(Value::Unit(units)) = env.get(s) {
                 if (*units).len() > 1 || (*units)[0].offset != rat!(0) {
@@ -107,7 +118,7 @@ fn try_get_unit(name: &str, env: &HashMap<String, Value>) -> Option<Unit> {
 /// Attempt to get the scale value of a [Prefix].
 pub fn try_get_prefix_scale(name: &str) -> Option<BigRational> {
     Some(
-        (*PREFIXES)
+        standard_prefixes()
             .iter()
             .find(|prefix| prefix.standalone() && prefix.text() == name)?
             .scale()
@@ -117,7 +128,7 @@ pub fn try_get_prefix_scale(name: &str) -> Option<BigRational> {
 
 macro_rules! prefixes {
     ( $( $name:literal $base:literal^$scale:literal $standalone:tt ),* $(,)? ) => {
-        vec![ $(
+        [ $(
             Prefix::new(
                 $name,
                 rat!($base).pow($scale),
@@ -127,85 +138,97 @@ macro_rules! prefixes {
     };
 }
 
-lazy_static! {
-    /// Standard prefixes for units.
-    /// Mostly derived from <https://frinklang.org/frinkdata/units.txt>.
-    pub static ref PREFIXES: Vec<Prefix> = prefixes![
-        "yotta"  10^24  y,
-        "zetta"  10^21  y,
-        "exa"    10^18  y,
-        "peta"   10^15  y,
-        "tera"   10^12  y,
-        "giga"    10^9  y,
-        "mega"    10^6  y,
-        "myria"   10^4  y,
-        "kilo"    10^3  y,
-        "hecto"   10^2  y,
-        "deca"    10^1  y,
-        "deka"    10^1  y,
-        "deci"   10^-1  y,
-        "centi"  10^-2  y,
-        "milli"  10^-3  y,
-        "micro"  10^-6  y,
-        "nano"   10^-9  y,
-        "pico"  10^-12  y,
-        "femto" 10^-15  y,
-        "atto"  10^-18  y,
-        "zepto" 10^-21  y,
-        "yocto" 10^-24  y,
+/// # Standard prefixes for units.
+/// 
+/// This function returns a static slice of [`Prefix`] items which makes up the
+/// list of standard unit prefixes.
+///
+/// Contains both [SI][1] and [IEC][2] prefixes.
+/// Mostly derived from <https://frinklang.org/frinkdata/units.txt>.
+///
+/// [1]: https://en.wikipedia.org/wiki/International_System_of_Units#Prefixes
+/// [2]: https://en.wikipedia.org/wiki/Byte#Units_based_on_powers_of_2
+pub fn standard_prefixes() -> &'static [Prefix] {
+    static PREFIXES: Lazy<[Prefix; 59]> = Lazy::new(|| {
+        prefixes![
+            "yotta"  10^24  y,
+            "zetta"  10^21  y,
+            "exa"    10^18  y,
+            "peta"   10^15  y,
+            "tera"   10^12  y,
+            "giga"    10^9  y,
+            "mega"    10^6  y,
+            "myria"   10^4  y,
+            "kilo"    10^3  y,
+            "hecto"   10^2  y,
+            "deca"    10^1  y,
+            "deka"    10^1  y,
+            "deci"   10^-1  y,
+            "centi"  10^-2  y,
+            "milli"  10^-3  y,
+            "micro"  10^-6  y,
+            "nano"   10^-9  y,
+            "pico"  10^-12  y,
+            "femto" 10^-15  y,
+            "atto"  10^-18  y,
+            "zepto" 10^-21  y,
+            "yocto" 10^-24  y,
 
-        "Y"  10^24   n, // yotta
-        "Z"  10^21   n, // zetta
-        "E"  10^18   n, // exa
-        "P"  10^15   n, // peta
-        "T"  10^12   n, // tera
-        "G"   10^9   n, // giga
-        "M"   10^6   n, // mega
-        "k"   10^3   n, // kilo
-        "h"   10^2   n, // hecto
-        "da"  10^1   n, // deka
-        "d"  10^-1   n, // deci
-        "c"  10^-2   n, // centi
-        "m"  10^-3   n, // milli
-        "µ"  10^-6   n, // micro
-        "u"  10^-6   n, // micro (ASCII alternative)
-        "n"  10^-9   n, // nano
-        "p"  10^-12  n, // pico
-        "f"  10^-15  n, // femto
-        "a"  10^-18  n, // atto
-        "z"  10^-21  n, // zepto
-        "y"  10^-24  n, // yocto
+            "Y"  10^24   n, // yotta
+            "Z"  10^21   n, // zetta
+            "E"  10^18   n, // exa
+            "P"  10^15   n, // peta
+            "T"  10^12   n, // tera
+            "G"   10^9   n, // giga
+            "M"   10^6   n, // mega
+            "k"   10^3   n, // kilo
+            "h"   10^2   n, // hecto
+            "da"  10^1   n, // deka
+            "d"  10^-1   n, // deci
+            "c"  10^-2   n, // centi
+            "m"  10^-3   n, // milli
+            "µ"  10^-6   n, // micro
+            "u"  10^-6   n, // micro (ASCII alternative)
+            "n"  10^-9   n, // nano
+            "p"  10^-12  n, // pico
+            "f"  10^-15  n, // femto
+            "a"  10^-18  n, // atto
+            "z"  10^-21  n, // zepto
+            "y"  10^-24  n, // yocto
 
-        "kibi"  1024^1  y,
-        "mebi"  1024^2  y,
-        "gibi"  1024^3  y,
-        "tebi"  1024^4  y,
-        "pebi"  1024^5  y,
-        "exbi"  1024^6  y,
-        "zebi"  1024^7  y,
-        "yobi"  1024^8  y,
+            "kibi"  1024^1  y,
+            "mebi"  1024^2  y,
+            "gibi"  1024^3  y,
+            "tebi"  1024^4  y,
+            "pebi"  1024^5  y,
+            "exbi"  1024^6  y,
+            "zebi"  1024^7  y,
+            "yobi"  1024^8  y,
 
-        "Ki"  1024^1  n, // kibi
-        "Mi"  1024^2  n, // mebi
-        "Gi"  1024^3  n, // gibi
-        "Ti"  1024^4  n, // tebi
-        "Pi"  1024^5  n, // pebi
-        "Ei"  1024^6  n, // exbi
-        "Zi"  1024^7  n, // zebi
-        "Yi"  1024^8  n, // yobi
-    ];
+            "Ki"  1024^1  n, // kibi
+            "Mi"  1024^2  n, // mebi
+            "Gi"  1024^3  n, // gibi
+            "Ti"  1024^4  n, // tebi
+            "Pi"  1024^5  n, // pebi
+            "Ei"  1024^6  n, // exbi
+            "Zi"  1024^7  n, // zebi
+            "Yi"  1024^8  n, // yobi
+        ]
+    });
+    &*PREFIXES
+}
 
-    /// List of suffixes which map to singular suffix translation.
-    ///
-    /// For example, "henries" is the plural of "henry" so the suffix "ies" maps to "y".
-    /// "bytes" is the plural of "byte", so the suffix "s" maps to nothing ("").
-    pub static ref SUFFIX_MAP: HashMap<String, String> = {
-        let mut map = HashMap::new();
-        map.insert("s".to_string(), "".to_string());
-        map.insert("es".to_string(), "".to_string());
-        map.insert("ies".to_string(), "y".to_string());
-        map
-    };
+/// # Standard suffixes for units
+/// 
+/// This function returns a static slice of string pairs.
+/// 
+/// Each pair contains a plural suffix and its equivalent singular form.
+///
+/// For example, `henries` is the plural of `henry` so the suffix `ies` maps to
+/// `y`. `bytes` is the plural of `byte`, where the suffix `s` maps to nothing
+/// (i.e. an empty string).
+pub fn standard_suffixes() -> &'static [(&'static str, &'static str)] {
+    &[("s", ""), ("es", ""), ("ies", "y")]
 }
 
 #[cfg(test)]
@@ -215,7 +238,7 @@ mod tests {
     /// Just test the first two prefixes to make sure the assignment works.
     #[test]
     fn std_prefixes() {
-        assert!(PREFIXES.contains(&Prefix::new("yotta", rat!(10).pow(24), true)));
-        assert!(PREFIXES.contains(&Prefix::new("k", rat!(1000), false)));
+        assert!(standard_prefixes().contains(&Prefix::new("yotta", rat!(10).pow(24), true)));
+        assert!(standard_prefixes().contains(&Prefix::new("k", rat!(1000), false)));
     }
 }
